ac1341545289b222ff30332a176f8b7ec73e8869
Saumitra Apte <saumitra.apte31@gmail.com>
2023-04-09 20:37:54 -0400

    logo updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React,{useState} from 'react';
 import {FaBars,FaTimes} from 'react-icons/fa';
 import logo from '../assets/logo.png';
+import logo1 from '../assets/logo1.jpeg'
 const Navbar=()=>{
     const [navbarOpen, setNavbarOpen] = useState(false);
     return(
         <div className='w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-green-300'>
             <div>
-              <img src={logo} alt="logo image" style={{width:'50px'}}/>
+              <img src={logo1} alt="logo image" style={{width:'50px'}}/>
             </div>
           
                 <ul className='hidden md:flex'>
